35406164702b89a732e6e816b53ef87b4f528e7e
karanpant <karanpant@outlook.com>
2017-10-03 08:52:44 +0545

    jpa entities and bootstrapping started

--- src/main/java/mydailyprocess/wordpress/com/spring5webapp/model/Book.java
+++ src/main/java/mydailyprocess/wordpress/com/spring5webapp/model/Book.java
@@ -8,7 +8,6 @@ import java.util.Set;
 public class Book {
     private String title;
     private String isbn;
-    private String publisher;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,24 +22,36 @@ public class Book {
     }
 
     @ManyToMany
-    @JoinTable(name="author_book", joinColumns = @JoinColumn(name="book_id"), inverseJoinColumns = @JoinColumn(name="author_id"))
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @OneToOne
+    private Publisher publisher;
+
 
     public Book() {
 
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
-        this.publisher = publisher;
         this.authors = authors;
+        this.publisher = publisher;
     }
 
     public String getTitle() {
@@ -57,14 +68,6 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
     }
 
     public Set<Author> getAuthors() {
